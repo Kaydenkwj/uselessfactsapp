72d2d63d8f09a3ca4117cb67d43eff3fc63c4198
Kayden <kaydenkwj@gmail.com>
2023-07-01 10:34:22 +0800

    Initial Commit

---
+++ useless facts app/useless_facts_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  useless_facts_appApp.swift
+//  useless facts app
+//
+//  Created by T Krobot on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct useless_facts_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
